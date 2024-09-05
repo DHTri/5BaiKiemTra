7e050cd96f81fcb38f06d5ac066c08f5291f8d01
DHTri <145561569+DHTri@users.noreply.github.com>
2024-09-05 13:35:47 +0700

    Update and rename README.md to T-Prime.cpp

---
+++ T-Prime.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int nt(int n)
+{
+    if(n < 2) 
+    return 0;
+    for(int i = 2;i<=sqrt(n);i++)
+        if(n%i == 0)
+            return 0;
+    return 1;
+}
+
+int kt(int n)
+{
+    int s = sqrt(n);
+    if(s*s == n && nt(s))
+        return 1;
+    return 0;
+}
+
+int main()
+{
+    int n;cin>>n;
+    int d = 0;
+    for(int i =4;i<=n;i++)
+        if(kt(i))
+            d++;
+    cout<<d;
+} 
